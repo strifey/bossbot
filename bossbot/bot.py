b3391886a8a08203b8ed551648ba6b7743bcfb93
Randal Brown <rwb100jr@gmail.com>
2020-10-11 16:47:35 -0700

    Add roll command

--- bossbot/bot.py
+++ bossbot/bot.py
@@ -161,3 +161,48 @@ async def finish_register_goodreads_user(bot, message):
 @on_command('reading')
 async def goodreads(bot, message):
     await gr_reading(bot, message)
+
+
+@on_command('roll')
+async def roll(bot, message):
+    split_message = message.content.split(maxsplit=2)
+    if len(split_message) < 3:
+        await message.channel.send('🐕')
+        return
+
+    totals = []
+    results_embed = Embed(title='Results')
+    rolls = split_message[2].split(',')
+    for roll in rolls:
+        roll_components = roll.strip().split('d')
+        if len(roll_components) != 2:
+            await message.channel.send('Improper format. Format rolls like `@bossbot roll 1d20`')
+            return
+        try:
+            die_count = int(roll_components[0])
+            die_size = int(roll_components[1])
+        except ValueError:
+            await message.channel.send('Improper format. Format rolls like `@bossbot roll 1d20`')
+            return
+        if die_count <= 0 or die_size <= 0:
+            results_embed.add_field(name=roll, value='No rolls.', inline=False)
+            continue
+        die_count = min(100, die_count)
+
+        results = []
+        for _ in range(die_count):
+            results.append(random.randint(1, die_size))
+        total = sum(results)
+        totals.append(total)
+
+        if die_count > 1:
+            results_str = '{0} = {1}'.format(
+                ' + '.join(list(map(str, results))), total)
+        else:
+            results_str = str(results[0])
+        results_embed.add_field(name=roll, value=results_str, inline=False)
+
+    if len(totals) > 1:
+        results_embed.add_field(name='Total', value='{0} = {1}'.format(
+            ' + '.join(list(map(str, totals))), sum(totals)))
+    await message.channel.send(embed=results_embed)
