965e082ec937ef8664b83f25ec7f715043593a47
Randal Brown <rwb100jr@gmail.com>
2020-10-12 13:26:37 -0700

    decrease sass frequency

--- bossbot/bot.py
+++ bossbot/bot.py
@@ -52,7 +52,7 @@ class BossBot(Client):
 
 
             elif self._is_ping(message.content):
-                if (message.author.id == 97558918720389120 and random.randint(1, 20) == 20):
+                if (message.author.id == 97558918720389120 and random.randint(1, 35) == 35):
                     await message.channel.send("go fuck yourself")
                     return
 
